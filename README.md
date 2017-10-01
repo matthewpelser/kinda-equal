615e161e7b1e4ba2e004352ea598662f4f8e2b68
matthewpelser <matthew@independentcode.com>
2017-10-01 20:02:32 +0100

    Add readme and lic

---
+++ README.md
@@ -0,0 +1,18 @@
+# kinda-equal
+
+## About
+A depth first model comparison utility for messy models.
+
+## Usage
+* Install with: `npm install kinda-equal --save`
+
+```
+
+```
+
+## Dependencies
+
+## Developer instructions
+
+## License
+MIT
--- rollup.config.js
+++ rollup.config.js
@@ -3,34 +3,27 @@ import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
-
+const dist = 'dist';
 export default [
-	// browser-friendly UMD build
 	{
 		entry: 'src/kinda-equal.js',
-		dest: pkg.browser,
+		dest: `${dist}/kinda-equal.umd.js`,
 		format: 'umd',
 		moduleName: 'kindaEqual',
 		plugins: [
 			babel({
 				exclude: 'node_modules/**'
 			 }),
-			resolve(), // so Rollup can find `ms`
-			commonjs() // so Rollup can convert `ms` to an ES module
+			resolve(),
+			commonjs()
 		]
 	},
-
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// the `targets` option which can specify `dest` and `format`)
 	{
 		entry: 'src/kinda-equal.js',
 		external: [],
 		targets: [
-			{ dest: pkg.main, format: 'cjs' },
-			{ dest: pkg.module, format: 'es' }
+			{ dest: `${dist}/index.js`, format: 'cjs' },
+			{ dest: `${dist}/kinda-equal.es.js`, format: 'es' }
 		]
 	}
 ];
