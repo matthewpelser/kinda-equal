a52c761acd1c3f99e2cd23edc839610fbc90d2e1
Matthew Pelser <matthew@independentcode.com>
2020-03-08 22:10:14 +0000

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-import pkg from './package.json';
 
 const dist = 'dist';
 export default [
